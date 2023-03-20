9d36cfba1b182dce2df612e411ebceb518624048
Realzanman817 <128156677+Realzanman817@users.noreply.github.com>
2023-03-20 12:09:47 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,29 +1,37 @@
-
 public class Card {//opens class
 
 	private int rank;
 	private String suit;
 	private String face;
+	private int x;
+	private int y;
 	
 	
-	public Card(String suitIn, String faceIn) {//open constructor
+	public Card(String suitIn, String faceIn, int xIn, int yIn) {//open constructor
 		
 		suit = suitIn;
 		face = faceIn;
-		
+		x = xIn;
+		y = yIn;
 	}//close constructor
 	
 	
-	public String getSuit() {//opens getSuit()
+	public String getSuit() {//get suit
 		
 		return suit;
 		
-	}//close getSuit()
+	}//get suit
 	
-	public String getFace() {//open getFace()
+	public String getFace() {//get face
 		
 		return face;
 		
-	}//close getFace()
+	}//get face
+	public int getx() {//get x
+		return x;
+	}//get x
+	public int gety() {//get y
+		return y;
+	}//get y
 	
 }//close class
