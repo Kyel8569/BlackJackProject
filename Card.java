b56e48beef7272aef5a2f17d51f23bca62f186ae
Kyle Roehl <kyle13336@gmail.com~>
2023-03-20 12:46:49 -0400

    Revert "Update Card.java"
    This reverts commit 39c734742160763139b8aa835dea5fade7c56628.
    
    Correct extra integers

--- Card.java
+++ Card.java
@@ -1,37 +1,29 @@
+
 public class Card {//opens class
 
 	private int rank;
 	private String suit;
 	private String face;
-	private int x;
-	private int y;
 	
 	
-	public Card(String suitIn, String faceIn, int xIn, int yIn) {//open constructor
+	public Card(String suitIn, String faceIn) {//open constructor
 		
 		suit = suitIn;
 		face = faceIn;
-		x = xIn;
-		y = yIn;
+		
 	}//close constructor
 	
 	
-	public String getSuit() {//get suit
+	public String getSuit() {//opens getSuit()
 		
 		return suit;
 		
-	}//get suit
+	}//close getSuit()
 	
-	public String getFace() {//get face
+	public String getFace() {//open getFace()
 		
 		return face;
 		
-	}//get face
-	public int getx() {//get x
-		return x;
-	}//get x
-	public int gety() {//get y
-		return y;
-	}//get y
+	}//close getFace()
 	
 }//close class
