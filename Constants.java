a5b0f49f952ffca64abfd8153e26ad12b178f769
Kyle Roehl <kyle13336@gmail.com~>
2023-03-21 12:28:26 -0400

    Added Constants and made DeckImage more efficient

--- Constants.java
+++ Constants.java
@@ -1,8 +1,11 @@
 public class Constants {
     public class CardConstants{
-        public static final int deckCount = 52;
-        public static final int suitCount = 4;
-        
-
+        public static final int DECK_COUNT = 52;
+        public static final int SUIT_COUNT = 4;
+        public static final int CARD_WIDTH = 51;
+        public static final int CARD_HEIGHT = 70;
+    }
+    public class SwingConstants{
+        public static final String imageFilePath = "libs/Cards.png";
     }
 }
---
+++ DeckImage.java
@@ -0,0 +1,30 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
+public class DeckImage {
+	static BufferedImage deckImage;
+
+	DeckImage(File image) {
+		try {
+			deckImage = ImageIO.read(image);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	BufferedImage getImage(int row, int column) {
+		int x = column * Constants.CardConstants.CARD_WIDTH;
+		int y = row * Constants.CardConstants.CARD_HEIGHT;
+
+		BufferedImage image = new BufferedImage(Constants.CardConstants.CARD_WIDTH, Constants.CardConstants.CARD_HEIGHT, BufferedImage.TYPE_INT_RGB);
+		Graphics g = image.createGraphics();
+
+		g.drawImage(deckImage, 0, 0, Constants.CardConstants.CARD_WIDTH, Constants.CardConstants.CARD_HEIGHT, x, y, x + Constants.CardConstants.CARD_HEIGHT, y + Constants.CardConstants.CARD_WIDTH, null);
+		return image;
+	}
+}
+
